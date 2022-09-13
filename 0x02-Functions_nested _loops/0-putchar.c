17cd33da40f6f1dd60ce1263b16db922358aa30d
jean-on-hub <87473040+jean-on-hub@users.noreply.github.com>
2022-09-13 17:51:10 +0000

    we go

---
+++ 0x02-Functions_nested _loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Description: prints _putchar, followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+    
+	printf("_putchar")
+    printf("\n")
+    return (0);
+}
