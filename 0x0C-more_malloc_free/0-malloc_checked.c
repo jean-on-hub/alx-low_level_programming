00eeea95ce12686cf806f7be2742ca4e8d36da9a
jean-on-hub <87473040+jean-on-hub@users.noreply.github.com>
2022-10-10 17:30:01 +0000

    hmmm

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function
+ *
+ * Description: allocates memory using malloc
+ *
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m = malloc(b);
+
+	if (m == NULL)
+		exit(98);
+
+	return (m);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function
+ *
+ * Description: allocates memory for an array
+ *
+ * @nmemb: number of elements
+ * @size: bytes of each element
+ *
+ * Return: NULL or pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+	char *f;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	m = malloc(size * nmemb);
+
+	if (m == NULL)
+		return (NULL);
+
+	f = m;
+
+	for (i = 0; i < (size * nmemb); i++)
+		f[i] = '\0';
+
+	return (m);
+}
