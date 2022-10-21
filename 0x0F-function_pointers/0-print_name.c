6a91073927a365cc2323d290ef42ab2bef81ee3e
jean-on-hub <87473040+jean-on-hub@users.noreply.github.com>
2022-10-21 16:24:10 +0000

    hw3

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function
+ * Description: prints a name
+ *
+ * @name: name to print
+ * @f: pointer to the printing function
+ *
+ * Return: void
+ */
+ 
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
